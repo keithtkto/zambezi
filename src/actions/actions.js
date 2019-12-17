d7ea53d7295ed1d557ad8692fd005e0daf1fa107
keith.to <keith.to@mediaartslab.com>
2019-12-16 20:39:22 -0800

    create welcome page

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -5,7 +5,7 @@ import htmlToText from "html-to-text";
 export const useGetIP = () => {
   const [{ data, loading, error }] = useAxios("https://ipapi.co/json/");
   const user = data
-    ? { ip: data.ip, lat: data.latitude, long: data.longitude }
+    ? { ip: data.ip, lat: data.latitude, long: data.longitude, city: data.city }
     : {};
 
   return [user, loading, error];
@@ -38,7 +38,7 @@ export const getRobot = async (ip, cb) => {
 
 export const getMapTile = async ({ lat, long }, cb) => {
   const data = await axios.get(
-    `https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/pin-s(${long},${lat})/${long},${lat},9.67,0.00,0.00/500x500@2x?access_token=pk.eyJ1Ijoic2VhbnAiLCJhIjoiY2p1MDY1YmljM2NnMjN6bXVnZXRpeGdvdSJ9.om-OkYVrL4-GyeWA3Evj2g`,
+    `https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/pin-s(${long},${lat})/${long},${lat},9.67,0.00,0.00/500x150@2x?access_token=pk.eyJ1Ijoic2VhbnAiLCJhIjoiY2p1MDY1YmljM2NnMjN6bXVnZXRpeGdvdSJ9.om-OkYVrL4-GyeWA3Evj2g`,
     { headers: { "Content-Type": "image/png" }, responseType: "blob" }
   );
 
---
+++ src/containers/Welcome.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Robot from "../components/Robot";
+import MapTile from "../components/MapTile";
+import "../styles/welcome.css";
+
+export default ({ map, robot, city }) => {
+  return (
+    <Link to="/main">
+      <h1>Welcome</h1>
+      <Robot robot={robot} />
+      <MapTile map={map} />
+      <p className="intro">
+        Hello human! I am your locally built Quote Bot from {city}. Please rate
+        the quote and I will fetch you a new one.
+        <br />
+        <br />
+        Click here to continue.
+      </p>
+    </Link>
+  );
+};
