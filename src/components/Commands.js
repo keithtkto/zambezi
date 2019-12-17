b360902481cc8008bd567fcd560c1f0ba10df7b5
keith.to <keith.to@mediaartslab.com>
2019-12-16 21:33:47 -0800

    change clickabble items to button

--- src/components/Commands.js
+++ src/components/Commands.js
@@ -5,14 +5,14 @@ export default ({ handleShowHistory, handleGetVoiceData }) => {
   return (
     <div>
       <h2>Robot Commands</h2>
-      <ul className="commands">
-        <li className="command" onClick={handleGetVoiceData}>
+      <div className="commands">
+        <button className="command" onClick={handleGetVoiceData}>
           Read this quote
-        </li>
-        <li className="command" onClick={handleShowHistory}>
+        </button>
+        <button className="command" onClick={handleShowHistory}>
           Show past quotes
-        </li>
-      </ul>
+        </button>
+      </div>
     </div>
   );
 };
