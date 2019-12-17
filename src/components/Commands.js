0a480af0f937059812fa345c4138b04df7b29527
keith.to <keith.to@mediaartslab.com>
2019-12-16 21:42:33 -0800

    disable audio when there's no code

--- src/components/Commands.js
+++ src/components/Commands.js
@@ -1,13 +1,17 @@
 import React from "react";
 import "../styles/commands.css";
 
-export default ({ handleShowHistory, handleGetVoiceData }) => {
+export default ({ handleShowHistory, handleGetVoiceData, hasQuote }) => {
   return (
     <div>
       <h2>Robot Commands</h2>
       <div className="commands">
-        <button className="command" onClick={handleGetVoiceData}>
-          Read this quote
+        <button
+          className="command"
+          onClick={hasQuote && handleGetVoiceData}
+          style={{ color: hasQuote ? "#000" : "grey" }}
+        >
+          {hasQuote ? "Read this quote" : "Awaiting new quote"}
         </button>
         <button className="command" onClick={handleShowHistory}>
           Show past quotes
